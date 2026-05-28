e05f68128fec3bfce4106f4a66291074fbb762af
david-vasquez-o <david.vasquez@oacg.cl>
2026-05-28 08:36:48 -0400

    test(dashboard): authorize sync integration test

--- Producto/backend/simfat-backend/src/test/java/com/simfat/backend/controller/DashboardControllerIntegrationTest.java
+++ Producto/backend/simfat-backend/src/test/java/com/simfat/backend/controller/DashboardControllerIntegrationTest.java
@@ -26,6 +26,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -63,6 +64,7 @@ class DashboardControllerIntegrationTest {
     }
 
     @Test
+    @WithMockUser(authorities = "PERM_DASHBOARD_SYNC_RUN")
     void runSync_returnsApiResponseWrappedPayload() throws Exception {
         SyncRunResponseDTO syncRunResponse = new SyncRunResponseDTO();
         syncRunResponse.setTotalRegions(1);
